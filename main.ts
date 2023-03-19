d5db8f103e2d91eabced51acbaeddb66a290d372
TheofilosGeo <theofilos.gewrgakis@gmail.com>
2023-03-19 14:22:24 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,6 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 20)
-    TankDistance = sonar.ping(
-    DigitalPin.P0,
-    DigitalPin.P0,
-    PingUnit.MicroSeconds
-    )
 })
-let TankDistance = 0
 basic.showIcon(IconNames.Yes)
 wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 180)
