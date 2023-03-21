b2479f3667836185e4449f614a13a24192b54706
TheofilosGeo <theofilos.gewrgakis@gmail.com>
2023-03-21 14:02:14 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,6 +12,18 @@ input.onButtonPressed(Button.A, function () {
         )
     }
     basic.showNumber(TankDistance)
+    basic.pause(2000)
+    if (TankDistance <= 5) {
+        value = 1
+        basic.showNumber(value)
+    } else if (TankDistance <= 7) {
+        value = 2
+        basic.showNumber(value)
+    } else {
+        value = 3
+        basic.showNumber(value)
+    }
 })
+let value = 0
 let TankDistance = 0
 basic.showIcon(IconNames.Yes)
